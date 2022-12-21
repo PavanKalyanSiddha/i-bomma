15041999223af59e699472f33578f571db6b34e7
Pavan <pavankumarnspt419@gmail.com>
2022-12-21 16:22:03 +0530

    vw

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <div className="bg-[#252525] p-5 text-gray-300  text-[14px] flex justify-between ">
+    <div className="bg-[#252525]">
+      <div className=" p-5 text-gray-300  text-[14px] flex justify-between lg:max-w-[90vw] mx-auto">
         <ul className="flex gap-2">
           <li>Contact |</li>
           <li> DMCA |</li>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -10,44 +10,44 @@ import { BiSearchAlt2 } from "react-icons/bi";
 
 const Navbar = () => {
   return (
-    <div className="bg-[#252525] flex  items-center justify-between  ">
-      <div className="p-4 flex items-center gap-[100px] ">
-      <AiOutlineMenu className="text-white text-[22px]  w-10 h-8 p-2  bg-[#65b643] sm:hidden"/>
-      <img src={logo} alt="" className=" w-40   "/>
-      
-      </div>
-      
-      <div className="hidden sm:flex">
-        <ul className="text-white gap-5 flex ml-2">
-          <div>
-            <li className="flex justify-center items-center gap-2" >
-              <HiOutlineUserGroup className="text-[#65b643]" />
-              <div className="text-[14px] text-gray-300">About</div>
-            </li>
-          </div>
-          <div>
-            <li className="flex justify-center items-center gap-2" >
-            <AiFillBug className="text-[#65b643]" />
-              <div  className="text-[14px] text-gray-300">Bug Report</div>
-            </li>
-          </div>
-          <div>
-            <li className="flex justify-center items-center gap-2" >
-                <MdForwardToInbox className="text-[#65b643]" />
-            
-              <div className="text-[14px] text-gray-300">Contact</div>
-            </li>
-          </div>
-        </ul>
-      </div>
-      <div className="hidden sm:flex md:mr-6 relative" >
-        <input type="text" placeholder="Search.." className="rounded-full text-gray-300 bg-[#333333]  right-3 w-full placeholder:pl-3" />
-        <BiSearchAlt2 className="absolute right-0  rounded-r-full w-6 h-6 p-1 text-white bg-[#65b643]"/>
-        </div>
-        <BiSearchAlt2 className="sm:hidden     mr-5 mt-0.9  w-10 h-8 p-2 text-white bg-[#65b643]"/>
-
+    <div className="bg-[#252525]"><div className=" flex  items-center justify-between  lg:max-w-[90vw] mx-auto">
+    <div className="p-4 flex items-center gap-[100px] ">
+    <AiOutlineMenu className="text-white text-[22px]  w-10 h-8 p-2  bg-[#65b643] sm:hidden"/>
+    <img src={logo} alt="" className=" w-40   "/>
+    
     </div>
-  );
+    
+    <div className="hidden sm:flex">
+      <ul className="text-white gap-5 flex ml-2">
+        <div>
+          <li className="flex justify-center items-center gap-2" >
+            <HiOutlineUserGroup className="text-[#65b643]" />
+            <div className="text-[14px] text-gray-300">About</div>
+          </li>
+        </div>
+        <div>
+          <li className="flex justify-center items-center gap-2" >
+          <AiFillBug className="text-[#65b643]" />
+            <div  className="text-[14px] text-gray-300">Bug Report</div>
+          </li>
+        </div>
+        <div>
+          <li className="flex justify-center items-center gap-2" >
+              <MdForwardToInbox className="text-[#65b643]" />
+          
+            <div className="text-[14px] text-gray-300">Contact</div>
+          </li>
+        </div>
+      </ul>
+    </div>
+    <div className="hidden sm:flex md:mr-6 relative" >
+      <input type="text" placeholder="Search.." className="rounded-full text-gray-300 bg-[#333333]  right-3 w-full placeholder:pl-3" />
+      <BiSearchAlt2 className="absolute right-0  rounded-r-full w-6 h-6 p-1 text-white bg-[#65b643]"/>
+      </div>
+      <BiSearchAlt2 className="sm:hidden     mr-5 mt-0.9  w-10 h-8 p-2 text-white bg-[#65b643]"/>
+
+  </div>
+</div>  );
 };
 
 export default Navbar;
